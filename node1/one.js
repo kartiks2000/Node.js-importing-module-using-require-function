fc1baebe8e65ee42195303af92d60112faa09c9a
kartiks2000 <46376692+kartiks2000@users.noreply.github.com>
2020-08-23 14:18:50 +0530

    Add files via upload

---
+++ node1/one.js
@@ -0,0 +1,7 @@
+const fs = require('fs')
+
+fs.writeFileSync('hi','This is a new file created by file system module in node.js.')
+
+console.log("Hello, this is node.js")
+console.log("we are importing 'fs' module using 'require()' function of node.js which is actually file system module of node.js it helps managing, changing,creating,editing etc of local file on the computer...")
+console.log(" we run it from terminal using ->  node <filename>  that is -> node one.js")
